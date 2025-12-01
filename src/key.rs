b110d7e0e81f29bede9a2309c5564a47c664edb5
Rexyz <2840722773@qq.com>
2025-12-01 11:18:19 +0800

    优化键盘事件处理，修复按键释放时集合清空逻辑

--- src/key.rs
+++ src/key.rs
@@ -4,10 +4,11 @@ pub mod key {
     use std::{
         any::Any,
         collections::HashSet,
+        sync::atomic::{AtomicBool, Ordering},
         sync::{Arc, Mutex},
     };
 
-    use device_query::{DeviceEvents, DeviceEventsHandler, Keycode, DeviceState, DeviceQuery};
+    use device_query::{DeviceEvents, DeviceEventsHandler, Keycode};
 
     /// 初始化键盘事件处理器
     pub fn init_handler() -> (DeviceEventsHandler, Box<dyn Any>, Box<dyn Any>) {
@@ -16,23 +17,26 @@ pub mod key {
 
         // 创建集合记录同一时间按下的按键
         let keys_set: Arc<Mutex<HashSet<String>>> = Arc::new(Mutex::new(HashSet::new()));
+        // 如果上一次事件是某按键被按下，则设置为 true，否则为 false
+        let last_down_flag = Arc::new(AtomicBool::new(false));
 
         let keys_set_clone = Arc::clone(&keys_set);
+        let last_down_flag_clone = Arc::clone(&last_down_flag);
         let press_key_guard = event_handler.on_key_down(move |key| {
             record_press_key(&key, &keys_set_clone);
+            last_down_flag_clone.store(true, Ordering::SeqCst);
         });
 
-        let device_state = DeviceState::new();
         let keys_set_clone_2 = Arc::clone(&keys_set);
+        let last_down_flag_clone_2 = Arc::clone(&last_down_flag);
         let release_key_guard = event_handler.on_key_up(move |key| {
             if let Ok(set) = keys_set_clone_2.lock() {
-                if !set.is_empty() {
-                    let mut keys = device_state.get_keys();
-                    keys.push(*key);
-                    println!("Currently pressed keys: {:?}", &keys);
+                if last_down_flag_clone_2.load(Ordering::SeqCst) {
+                    println!("Currently pressed keys: {:?}", set);
                 }
             }
-            record_release_key(&keys_set_clone_2);
+            record_release_key(&key, &keys_set_clone_2);
+            last_down_flag_clone_2.store(false, Ordering::SeqCst);
             if let Keycode::Escape = key {
                 exit(0);
             }
@@ -52,9 +56,9 @@ pub mod key {
     }
 
     /// 有按键被释放时清空集合
-    fn record_release_key(keys_set: &Arc<Mutex<HashSet<String>>>) {
+    fn record_release_key(key: &Keycode, keys_set: &Arc<Mutex<HashSet<String>>>) {
         if let Ok(mut set) = keys_set.lock() {
-            set.clear();
+            set.remove(&key.to_string());
         }
     }
 }
